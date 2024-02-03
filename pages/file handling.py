5fe0e920a81026b67c9193b934be20ce6c3ed0e8
mayuresh mane <mane.mayuresh@gmail.com>
2024-02-03 14:32:01 +0530

    moving files

--- pages/file handling.py
+++ pages/file handling.py
@@ -9,3 +9,5 @@ max_col = sheet_obj.max_column
 for i in range(1, max_col+1):
     cell_obj = sheet_obj.cell(row = 1, column = i)
     print(cell_obj.value)
+
+
--- test/Login_test.py
+++ test/Login_test.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
-from tests.BaseTest import BaseTest
+from test.BaseTest import BaseTest
 
 
 class TestClass(BaseTest):
@@ -14,6 +13,7 @@ class TestClass(BaseTest):
         self.wait.until(EC.presence_of_element_located(self.username_locator)).send_keys("standard_user")
         self.wait.until(EC.presence_of_element_located(self.password_locator)).send_keys("secret_sauce")
         self.wait.until(EC.element_to_be_clickable(self.lgn_btn_locator)).click()
+
 
     def test_login_with_locked_out_user(self):
         self.wait.until(EC.presence_of_element_located(self.username_locator)).send_keys("locked_out_user")
